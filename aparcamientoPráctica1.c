5b86c9fecb5e02b2c073098a8715622783ce424b
mariabosaba <36692966+mariabosaba@users.noreply.github.com>
2018-03-13 19:22:53 +0100

    Mejora del código y apartado de avanzado.

--- aparcamientoPráctica1.c
+++ aparcamientoPráctica1.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include<stdlib.h>
+#include <string.h>
+#include <time.h>
+#define TARIFA 0.25
 
-void main() {
-	system("color 0F");
+int main() {
 	char opcion = 1, matricula1[8], matricula2[8] = ("1234ABCD");//Inicializo una de las matriculas, para tener una plaza llena.
 	int plaza1 = 0, plaza2 = 1;
+	float precio;
+	clock_t times = clock(); 
+		//hora
+    time_t ahora;
+    struct tm *hora;
+    time(&ahora);
+    hora = localtime(&ahora);
+    printf("Hora de entrada %d:%d:%d\n", hora->tm_hour, hora->tm_min, hora->tm_sec);
 	while (1) {
-		system("color 0F");
+		system("color 0B");
 		printf("BIENVENIDO AL APARCAMIENTO\n");
 		printf("MENU PRINCIPAL: \n");
 		printf("Seleccione una de estas opciones: \n");
@@ -14,21 +24,20 @@ void main() {
 		printf("A - Abandonar plaza. \n");
 		printf("E - Estado del aparcamiento. \n");
 		scanf_s("%c", &opcion);
-
 		switch (opcion) {
 		case'R':
 		case'r':
-			printf("Introduzca numero de matricula: \n");
+			getchar();
+		    printf("Introduzca numero de matricula:\n ");
 			if (plaza1 == 0) {
+				gets(matricula1);
 				printf("La plaza 1 esta libre. \n");
 				getchar();
-				gets(matricula1);
 				plaza1 = 1;
 			}
 			else if (plaza2 == 0) {
-				printf("La plaza 2 esta libre. \n");
-				getchar();
 				gets(matricula2);
+				printf("La plaza 2 esta libre. \n");
 				plaza2 = 1;
 			}
 			else {
@@ -42,13 +51,28 @@ void main() {
 				getchar();
 				strcpy(matricula1, "\0");
 				printf("La plaza 1 ya esta libre. Gracias por su visita!\n");
+				hora = localtime(&ahora);
+                                printf("Hora de salida %d:%d:%d\n", hora->tm_hour, hora->tm_min, hora->tm_sec);
+				times = (clock() - times)/1000; 
+                                precio=TARIFA*times;
+                                printf("El precio del aparcamiento es de 0.25 EUROS por segundo. \n");
+                                printf("Su vehiculo a permanecido en nuestro establecimiento %d segundos.\n",times);
+                                printf("El importe es de:%.2f euros.\n",precio);
+                
 			}
 			else if (plaza2 == 1) {
 				plaza2 = 0;
 				getchar();
 				strcpy(matricula2, "\0");
 				printf("La plaza 2 ya esta libre. Gracias por su visita!\n");
-			}
+				hora = localtime(&ahora);
+                                printf("Hora de salida %d:%d:%d", hora->tm_hour, hora->tm_min, hora->tm_sec);
+				times = (clock() - times)/1000; 
+                                precio=TARIFA*times;
+                                printf("El precio del aparcamiento es de 0.25 EUROS por segundo. \n");
+                                printf("Su vehiculo a permanecido en nuestro establecimiento %d segundos.\n",times);
+                                printf("El importe es de:%.2f\n",precio);
+			        }
 			else {
 				getchar();
 				printf("Las dos plazas estan libres\n");
